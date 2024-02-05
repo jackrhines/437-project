ae03d0a89cfd16cc1d7a2a25c30736f41b19ff9b
jackrhines <jrhines@calpoly.edu>
2024-02-05 10:21:35 -0800

    finished adding art cards to index

--- packages/lit-frontend/components/art-card.ts
+++ packages/lit-frontend/components/art-card.ts
@@ -7,7 +7,7 @@ export class ArtCard extends LitElement {
     src: string = ""
 
     @property()
-    url: string = ""
+    url: string = "/app"
 
     @property()
     title: string = ""
@@ -15,14 +15,21 @@ export class ArtCard extends LitElement {
     @property()
     artist: string = ""
 
+    @property()
+    kudos: number = 0
+
     render() {
         return html`
             <div class="card" href=${this.url}>
-                <img src=${this.src} alt=${this.title} width="200" height="200" />
-                <div class="container">
-                    <strong>${this.title}</strong>
-                    <p>by ${this.artist}</p>
-                </div>
+                <a href=${this.url}>
+                    <img src=${this.src} alt=${this.title} width="200" height="200" />
+                    <div class="container">
+                        <strong>${this.title}</strong>
+                        <p>${this.artist}</p>
+<!--                        <i>${this.kudos} kudos</i>-->
+                    </div>
+                </a>
+
             </div>
         `;
     }
@@ -31,19 +38,34 @@ export class ArtCard extends LitElement {
       .card {
         box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
         transition: 0.3s;
-        width: 210px;
-        padding: 0.5em;
-        background: var(--color-background-secondary)
+        width: 200px;
+        padding-bottom: 0.5em;
+        background: var(--color-background-secondary);
+        border-radius: 5px;
       }
       
       .card:hover {
-        box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+        box-shadow: 0 16px 24px 0 rgba(0,0,0,0.5);
       }
       
       .container {
         padding: 2px 16px;
         display: flex;
         flex-direction: column;
+      }
+      
+      a {
+        text-decoration: none;
+        color: black;
+      }
+      
+      p {
+        margin: 0;
+      }
+
+      img {
+        border-radius: 5px 5px 0 0;
+        background: white;
       }
     `
 }
