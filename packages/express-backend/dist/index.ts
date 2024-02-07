9662a66f261409955da5a98860da2b3b93b4b1cf
jackrhines <jrhines@calpoly.edu>
2024-02-07 10:42:05 -0800

    pe-4: set up express backend & connect to mongoDB

---
+++ packages/express-backend/dist/index.ts
@@ -0,0 +1,20 @@
+// src/index.ts
+import express, { Request, Response } from "express";
+import cors from "cors";
+import { connect } from "./MongoConnect";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/hello", (req: Request, res: Response) => {
+  res.send("Hello, World");
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
+connect("gallery"); // use your own db name here
