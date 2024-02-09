e334acca6f455090574764b6a1b7feec50907711
jackrhines <jrhines@calpoly.edu>
2024-02-09 10:38:11 -0800

    pe-4: finish server & database setup

--- packages/express-backend/dist/index.ts
+++ packages/express-backend/dist/index.ts
@@ -1,7 +1,8 @@
-// src/index.ts
 import express, { Request, Response } from "express";
 import cors from "cors";
 import { connect } from "./MongoConnect";
+import profiles from "./profiles";
+import { Profile } from "./models/profile";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,6 +12,24 @@ app.use(express.json());
 
 app.get("/hello", (req: Request, res: Response) => {
   res.send("Hello, World");
+});
+
+app.get("/api/profile/:userid", (req: Request, res: Response) => {
+  const { userid } = req.params;
+
+  profiles
+      .get(userid)
+      .then((profile: Profile) => res.json(profile))
+      .catch((err) => res.status(404).end());
+});
+
+app.post("/api/profiles", (req: Request, res: Response) => {
+  const newProfile = req.body;
+
+  profiles
+      .create(newProfile)
+      .then((profile: Profile) => res.status(201).send(profile))
+      .catch((err) => res.status(500).send(err));
 });
 
 app.listen(port, () => {
---
+++ packages/express-backend/dist/models/mongo/profile.ts
@@ -0,0 +1,18 @@
+import { Schema, Model, Document, model } from "mongoose";
+import { Profile } from "../Profile";
+
+const profileSchema = new Schema<Profile>(
+    {
+        userid: { type: String, required: true, trim: true },
+        name: { type: String, required: true, trim: true },
+        nickname: { type: String, trim: true },
+        city: { type: String, trim: true },
+        genres: [String],
+        mediums: [String]
+    },
+    { collection: "user_profiles" }
+);
+
+const ProfileModel = model<Profile>("Profile", profileSchema);
+
+export default ProfileModel;
